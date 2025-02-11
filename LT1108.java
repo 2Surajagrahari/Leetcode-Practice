573d607af58055f863edf209869d9f971264e1c1
Suraj Agrahari <surajagrahari265@gmail.com>
2025-02-11 21:36:09 +0530

    Second way

--- LT1108.java
+++ LT1108.java
@@ -1,6 +1,19 @@
 class Solution {
     public String defangIPaddr(String address) {
         String res = address.replace(".","[.]");
+
+        // second method
+
+         String res = "";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i) == '.'){
+                res += "[.]";
+            }
+            else{
+                res += address.charAt(i);
+            }
+        }
         return res;
+        
     }
 }
