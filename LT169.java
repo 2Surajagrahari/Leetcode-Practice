74c809d9d2ac1359aac4570c75bab86664ff95c6
Suraj Agrahari <surajagrahari265@gmail.com>
2025-03-27 12:37:31 +0530

    solve in O(n) time

--- LT169.java
+++ LT169.java
@@ -1,14 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // int res = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     int mid = nums.length/2;
+        //     res = nums[mid];
+        // }
+
+        // => O(n) time
+        // O(n) time 
+
         int res = 0;
+        int count = 0;
         for(int i=0;i<nums.length;i++){
-            int mid = nums.length/2;
-            res = nums[mid];
+            if(count == 0){
+                res = nums[i];
+            }
+            if(nums[i] == res){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-
-
-        
         return res;
     }
+
+    
 }
