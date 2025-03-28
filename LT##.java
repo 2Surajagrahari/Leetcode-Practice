6b0ad535a21c60449af08f2e9d7292f99212b977
shreyamrinal <152470392+Shreyamrinal@users.noreply.github.com>
2025-03-28 11:24:31 +0530

    Add contents

--- LT##.java
+++ LT##.java
@@ -2,4 +2,12 @@ Java is a widely used object-oriented programming language and software platform
 
 One major advantage of developing software with Java is its portability. Once you wrote code for a Java program on a notebook computer, it can be easily moved to a mobile device. When the language was invented in 1991 by James Gosling of Sun Microsystems (later acquired by Oracle), the primary goal was to be able to "write once, run anywhere."
 
-It's also important to understand that Java is much different from JavaScript. JavaScript does not need to be compiled, while Java code needs to be. Also, Javascript only runs on web browsers, while Java can be run anywhere.
+It's also important to understand that Java is much different from JavaScript. JavaScript does not need to be compiled, while Java code needs to be. Also, Javascript only runs on web browsers, while Java can be run anywhere.'
+
+
+How Java works
+Before exploring why it remains popular, let us review what Java is in more detail and its importance for enterprise application development.
+
+Java is a technology that uses both a programming language and a software platform. To create an application using Java, you need to download the Java Development Kit (JDK), which is available for Windows, macOS and Linux®. You write the program in the Java programming language, then a compiler turns the program into Java bytecode—the instruction set for the Java Virtual Machine (JVM) that is a part of the Java runtime environment (JRE). Java bytecode runs without modification on any system that supports JVMs, allowing your Java code to be run anywhere.
+
+The Java software platform consists of the JVM, the Java API and a complete development environment. The JVM parses and runs or interprets the Java bytecode. The Java API comprises an extensive set of libraries that include basic objects, networking and security functions, Extensible Markup Language (XML) generation, and web services. Taken together, the Java language and the Java software platform create a powerful, proven technology for enterprise software development.
